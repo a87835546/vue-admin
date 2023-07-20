9c35e9a2aa427d5d4c39d6f11f78b91952968224
yicen <yicen.yan@skig.tech>
2023-07-20 14:14:29 +0800

    add menu

--- src/api/table.js
+++ src/api/table.js
@@ -2,8 +2,31 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    url: '/vue-admin-template/table/list',
+    url: '/billboard/list',
     method: 'get',
     params
+  })
+}
+export function getMuneList(params) {
+  return request({
+    url: '/menu/list',
+    method: 'get',
+    params
+  })
+}
+
+export function addMune(data) {
+  return request({
+    url: '/menu/insert',
+    method: 'post',
+    data
+  })
+}
+
+export function deleteMune(data) {
+  return request({
+    url: '/menu/delete',
+    method: 'post',
+    data
   })
 }
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -18,14 +18,14 @@
           {{ scope.row.title }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Url">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          {{ scope.row.url }}
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="category_id" width="110" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          <span>{{ scope.row.category_id }}</span>
         </template>
       </el-table-column>
       <el-table-column class-name="status-col" label="Status" width="110" align="center">
@@ -33,10 +33,9 @@
           <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column align="center" prop="created_at" label="Display_time" width="250">
         <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <span>{{ scope.row.created_at }}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -70,7 +69,8 @@ export default {
     fetchData() {
       this.listLoading = true
       getList().then(response => {
-        this.list = response.data.items
+        console.log(response)
+        this.list = response.data
         this.listLoading = false
       })
     }
